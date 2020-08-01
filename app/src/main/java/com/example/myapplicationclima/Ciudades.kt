64e0662b21b55ecf99963ca6025ad51b6b640cf5
Alexis <omaroquendo221@gmail.com>
2020-07-31 19:05:49 -0500

    Se agregaron intents para la consulta de infromación. Close #5

--- app/src/main/java/com/example/myapplicationclima/Ciudades.kt
+++ app/src/main/java/com/example/myapplicationclima/Ciudades.kt
@@ -1,11 +1,25 @@
 package com.example.myapplicationclima
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class Ciudades : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ciudades)
+        val button_medellin = findViewById<Button>(R.id.button_medellin)
+        val button_cali = findViewById<Button>(R.id.button_cali)
+        button_medellin.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        })
+        button_cali.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
